efe035d11244ab7a0173f2342b85711d06e67ab3
wchb2017 <wchb20155@gmail.com>
2017-07-10 06:57:14 +0800

    update

---
+++ src/main/java/com/wchb/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.wchb.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    public String say(){
+        return 
+    }
+}
