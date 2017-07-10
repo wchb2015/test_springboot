8531086b01dcb880dd9d4b943f9b0a701516a978
wchb2017 <wchb20155@gmail.com>
2017-07-10 08:53:23 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # test_springboot
 springboot demo
+
+#### 启动
+1. mvn spring-boot:run
+2. ① mvn install  ② cd target  ③java -jar xxx.jar --spring.profiles.active=prod
+
+
+#### Tips
+1. @RestController = @Controller + @ResponseBody 返回json数据.
+2.
+ header 1 | header 2
+ ---|---
+ @PathVariable | 获取url中的数据
+ @RequestParam | 获取请求参数的值(GET和POST)
+ @GetMapping | 组合注解
--- src/main/java/com/wchb/controller/HelloController.java
+++ src/main/java/com/wchb/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.wchb.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${mobileNumber}")
+    private String mobileNumber;
 
-    public String say(){
-        return 
+    @RequestMapping(value = {"hi", "hello"}, method = RequestMethod.GET)
+    public String say() {
+        return "hello spring boot!!!" + mobileNumber;
+//        return "index";
     }
 }
